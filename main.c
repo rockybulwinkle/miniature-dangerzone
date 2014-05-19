02a6351d6942874abddb42dcb76f3c4b6b9b0850
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 11:14:34 -0400

    added benchmarking stuff

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
+#include <time.h>
+#include <stdio.h>
 #include "structs.h"
 #include "network_functions.h"
 
 int main(int argc, char * argv[]){
 //	const int layer_size[] = {1,2,1};
 //	Network * network = alloc_network(3, layer_size);
-	Network * network = load_network("default.net");
+	Network * network = load_network(argv[1]);
 	network->layer[0].node[0].output=1;
-	network->layer[0].node[1].output=6.92;
-	evaluate_network(network);
+	int i;
+	clock_t start_time = clock();
+	for (i = 0; i < 10; i++){
+		evaluate_network(network);
+	}
+	double elapsed = (clock()-start_time)/(CLOCKS_PER_SEC*1.0f);
 	print_network_layer_output(network, network->size-1);
+	printf("%f\n", elapsed);
 	save_network(network, "default.net");
 	free_network(network);
 	return 0;
---
+++ make_random_net.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+f = open("random.net", "w")
+layer_format=[1, 1000, 1000, 1]
+f.write("%d\n"%(len(layer_format)))
+f.write(" ".join(map(str,layer_format)))
+f.write("\n\n")
+
+layer = 1
+while layer < len(layer_format):
+	for node in range(layer_format[layer]):
+		for weight in range(layer_format[layer-1]):
+			f.write("0.000\t")
+		f.write("\n0\n")
+	f.write("\n")
+	layer+=1
+	
