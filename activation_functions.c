e08d30d92298938ea34cb7da54257fd8ecc41a0c
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 15:13:03 -0400

    speed improvements by only dereferencing pointers once.

--- activation_functions.c
+++ activation_functions.c
@@ -2,10 +2,14 @@
 
 void LinearActivation(Network * network, Layer * previous_layer, Node * self){
 	int node_index;
-	network->net[self->node_offset]=0;
+	double * net = &(network->net[self->node_offset]);
+	*net = 0;
+	double * prev_output = &(network->output[previous_layer->node[0].weight_offset]);
+	double * weight = &(network->weight[self->weight_offset]);
+
 	for (node_index=0; node_index < previous_layer->size; node_index++){
-		network->net[self->node_offset] += network->output[previous_layer->node[node_index].node_offset]*network->weight[self->weight_offset+node_index];
+		*net += weight[node_index]*prev_output[node_index];
 	}
-	network->output[self->node_offset] = network->net[self->node_offset];
+	network->output[self->node_offset] = *net;
 }
 
--- activation_functions.h
+++ activation_functions.h
@@ -1,7 +1,7 @@
 #ifndef ACTIVATION_FUNCTIONS_H
 #define ACTIVATION_FUNCTIONS_H
 #define LINEAR_ACTIVATION 0
-void LinearActivation(Network * network, Layer * previous_layer, Node * self);
+void  LinearActivation(Network * network, Layer * previous_layer, Node * self);
 
 void (*ACTIVATION_FUNCTION[])(Network * network, Layer *, Node *) = {LinearActivation};
 const void (*TRAINING_FUNCTION[])(Network *, int, int) = {};
