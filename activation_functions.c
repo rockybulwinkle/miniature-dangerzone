90d2d3febd944f9954f2dedf9b21d2274160b739
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 16:29:48 -0400

    removed debug output

--- activation_functions.c
+++ activation_functions.c
@@ -8,11 +8,11 @@ void LinearActivation(Network * network, int layer, Node * self){
 
 	double * weight = &(network->weight[self->weight_offset]);
 
-	printf("node_offset=%d\n", self->node_offset);
+//	printf("node_offset=%d\n", self->node_offset);
 	for (node_index=0; node_index < network->layer[layer-1].size; node_index++){
 		*net += weight[node_index]*prev_output[node_index];
 
-		printf("%f*%f\n", prev_output[node_index], weight[node_index]);
+//		printf("%f*%f\n", prev_output[node_index], weight[node_index]);
 	}
 	network->output[self->node_offset] = *net;
 }
