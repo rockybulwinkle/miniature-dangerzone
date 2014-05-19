e284476ac0c9fd9b21014e68557e6d87d6c5bb0b
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 16:28:01 -0400

    remove debug output

--- activation_functions.c
+++ activation_functions.c
@@ -5,7 +5,7 @@ void LinearActivation(Layer * previous_layer, Node * self){
 	self->net=0;
 	for (node_index=0; node_index < previous_layer->size; node_index++){
 		self->net += previous_layer->node[node_index].output*self->weight[node_index];
-		printf("%f*%f\n", previous_layer->node[node_index].output, self->weight[node_index]);
+//		printf("%f*%f\n", previous_layer->node[node_index].output, self->weight[node_index]);
 	}
 	self->output = self->net;
 }
