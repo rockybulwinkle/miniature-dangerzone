4d039c9815526db4966cee1f9bb81858c6e94ea6
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 15:36:30 -0400

    debug output

--- activation_functions.c
+++ activation_functions.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include "structs.h"
-
 void LinearActivation(Layer * previous_layer, Node * self){
 	int node_index;
 	self->net=0;
 	for (node_index=0; node_index < previous_layer->size; node_index++){
 		self->net += previous_layer->node[node_index].output*self->weight[node_index];
+		printf("%f*%f\n", previous_layer->node[node_index].output, self->weight[node_index]);
 	}
 	self->output = self->net;
 }
