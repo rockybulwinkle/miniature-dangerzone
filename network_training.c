1c7c0194c8db1108f59ef45a8bdcba0093f469ca
Thriron <thriron9@gmail.com>
2014-05-18 23:08:22 -0400

    some changes

--- network_training.c
+++ network_training.c
@@ -7,10 +7,17 @@ void LinearTraining(Network* network, int layerNum, int nodeNum){
 	Layer layer = network->layer[layerNum];
 	Node node = layer.node[nodeNum];
 	int i;
+	double output = network->layer[network->size -1].node[0].output;
 	if(layer == size-1){ //output
 		for(i=0; i<layer.size; i++){
 			node.delta_weight[i] = stuff;
 		}
 	} 
+	if(layer == size-2){ //last hidden unit
+		for(i=0; i<layer.size; i++){
+			node.delta_weight[i] = stuff;
+		}
+	}
+}
 
 
