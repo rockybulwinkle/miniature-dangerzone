4093f96b985279889e2bf5160e889996aaa13f35
Chris Hopwood <rockybulwinkle@gmail.com>
2014-05-19 16:32:21 -0400

    make weights random in make_random_net.py

--- make_random_net.py
+++ make_random_net.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-
+import random
 f = open("random.net", "w")
 layer_format=[1, 2000, 2000, 1]
 f.write("%d\n"%(len(layer_format)))
@@ -10,7 +10,7 @@ layer = 1
 while layer < len(layer_format):
 	for node in range(layer_format[layer]):
 		for weight in range(layer_format[layer-1]):
-			f.write("0.000\t")
+			f.write("%f\t"%random.random())
 		f.write("\n0\n")
 	f.write("\n")
 	layer+=1
